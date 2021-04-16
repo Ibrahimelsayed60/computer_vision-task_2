612e199f44a2f7cb8386caeaebe3a40fd4537c93
mariembador <43917000+mariembador@users.noreply.github.com>
2021-04-17 00:02:58 +0200

    Area & Perimeter

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import cv2
 import snake as sn
 import math
 import pyqtgraph as pg
-
+import numpy as np
 class ApplicationWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(ApplicationWindow, self).__init__()
@@ -22,9 +22,9 @@ class ApplicationWindow(QtWidgets.QMainWindow):
 
         file_to_load = "images/example.jpg"
         self.image_1 = cv2.imread( file_to_load, cv2.IMREAD_COLOR )
-        self.ui.pushButton.clicked.connect(self.open_canny_tab)
-        self.ui.pushButton_2.clicked.connect(self.open_Hough_tab)
-        self.ui.pushButton_3.clicked.connect(self.open_Active_contour_tab)
+        # self.ui.pushButton.clicked.connect(self.open_canny_tab)
+        # self.ui.pushButton_2.clicked.connect(self.open_Hough_tab)
+        # self.ui.pushButton_3.clicked.connect(self.open_Active_contour_tab)
         self.ui.pushButton_4.clicked.connect(self.active_contour)
         self.ui.pushButton_5.clicked.connect(self.clearbutton)
 
@@ -51,7 +51,17 @@ class ApplicationWindow(QtWidgets.QMainWindow):
             snakeImg = snake.visuaize_Image()
             img = pg.ImageItem(snakeImg)
             self.ui.image_1.addItem(img)
-            
+            x = []
+            y = []
+            for i in range(len(snake.points)):
+            	x.append(snake.points[i][0])
+            	y.append(snake.points[i][1])
+            area=0.5*np.sum(y[:-1]*np.diff(x) - x[:-1]*np.diff(y))
+            area=np.abs(area)
+            perimeter = snake.get_length()
+            self.ui.textEdit.setPlaceholderText("{}".format(area/10000))
+            self.ui.textEdit_2.setPlaceholderText("{}".format(perimeter/100))
+            # self.ui.image_1.setTitle("area = {} , perimeter = {}".format(area, perimeter))
             snake_changed = snake.step()
 
             # Stops looping when ESC pressed
